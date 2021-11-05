c1988d68fc97f37c60f479d330f21e8f4c794472
Jakob Harder Poulsen <jakobharderp@gmail.com>
2021-11-05 17:25:28 +0100

    Updated

--- bots/BotimusPrime.py
+++ bots/BotimusPrime.py
@@ -2,9 +2,12 @@
 import random
 from typing import Sequence
 
+from environment.Constants import Action, Stage
+
 from bots.BotInterface import BotInterface
 from environment.Constants import Action
 from environment.Observation import Observation
+from utils.handValue import getHandPercent,getBoardHandType
 
 # your bot class, rename to match the file name
 class BotimusPrime(BotInterface):
@@ -24,7 +27,35 @@ class BotimusPrime(BotInterface):
                     action (Action): the action you want you bot to take. Possible actions are: FOLD, CHECK, CALL and RAISE
             If this function takes longer than 1 second, your bot will fold
         '''
+        
+         # use different strategy depending on pre or post flop (before or after community cards are delt)
+        stage = observation.stage
+        if stage == Stage.PREFLOP:
+            return self.handlePreFlop(observation)
 
-        # do a random action
-        action = random.choice(action_space)
-        return action
+        return self.handlePostFlop(observation)
+
+    def handlePreFlop(self, observation: Observation) -> Action:
+        # get my hand's percent value (how good is this 2 card hand out of all possible 2 card hands)
+        handPercent, _ = getHandPercent(observation.myHand)
+        # if my hand is top 20 percent: raise
+        if handPercent < .20:
+            return Action.RAISE
+        # if my hand is top 60 percent: call
+        elif handPercent < .60:
+            return Action.CALL
+        # else fold
+        return Action.FOLD
+
+    def handlePostFlop(self, observation: Observation) -> Action:
+        # get my hand's percent value (how good is the best 5 card hand i can make out of all possible 5 card hands)
+        handPercent, cards = getHandPercent(
+            observation.myHand, observation.boardCards)
+        # if my hand is top 30 percent: raise
+        if handPercent <= .30:
+            return Action.RAISE
+        # if my hand is top 80 percent: call
+        elif handPercent <= .40:
+            return Action.CALL
+        # else fold
+        return Action.FOLD
